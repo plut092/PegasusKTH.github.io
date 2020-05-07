4860823b4a7cf1c91e909d9c024085ce6e5605f9
joja3228 <joja3228@student.su.se>
2020-05-07 10:29:54 +0200

    fixt so it displays list

--- js/main.js
+++ js/main.js
@@ -34,6 +34,8 @@ var config = {
 
 };
 
+var listIntegration;
+
 // exports node tree to prereqList.js
 function getRootNode() {
 	return nodifyLookupMAIN("DD2420").buildTree();
@@ -43,7 +45,7 @@ function getRootNode() {
 function firstCall(courseArgument){
 
 	// listIntegration has taken jsonObject and converted it into a tree of nodes
-	var listIntegration = nodifyLookupMAIN(courseArgument).buildTree(); // SearchBox integration needed.
+	listIntegration = nodifyLookupMAIN(courseArgument).buildTree(); // SearchBox integration needed.
 
 	// assigns a unique ID to every node in the tree
 	listIntegration.assignIdentifiers([]);
